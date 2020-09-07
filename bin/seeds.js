5c736b020f6443a6f60f16d3f5490bdc32a8c1aa
edvicaty <edvicaty@hotmail.com>
2020-09-07 15:28:07 -0500

    add puppeteer basics

--- bin/seeds.js
+++ bin/seeds.js
@@ -4,46 +4,30 @@
 // $ node bin/seeds.js
 
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const User = require("../models/User");
-
-const bcryptSalt = 10;
+const Product = require("../models/Product");
+const puppeteer = require("puppeteer");
+require("dotenv").config();
 
 mongoose
-  .connect("mongodb://localhost/merca-app", { useNewUrlParser: true })
+  .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
     );
+    scrapWebPage();
   })
   .catch((err) => {
     console.error("Error connecting to mongo", err);
   });
 
-let users = [
-  {
-    email: "alice",
-    password: bcrypt.hashSync("alice", bcrypt.genSaltSync(bcryptSalt)),
-  },
-  {
-    email: "bob",
-    password: bcrypt.hashSync("bob", bcrypt.genSaltSync(bcryptSalt)),
-  },
-];
+async function scrapWebPage() {
+  let mainUrl = "";
+  let browser = await puppeteer.launch();
+  let page = await browser.newPage();
 
-User.deleteMany()
-  .then(() => {
-    return User.create(users);
-  })
-  .then((usersCreated) => {
-    console.log(`${usersCreated.length} users created with the following id:`);
-    console.log(usersCreated.map((u) => u._id));
-  })
-  .then(() => {
-    // Close properly the connection to Mongoose
-    mongoose.disconnect();
-  })
-  .catch((err) => {
-    mongoose.disconnect();
-    throw err;
+  await page.goto(mainUrl, { waitUntil: "networkidle2" });
+  let data = await page.evaluate(() => {
+    //queries and stuff to do
   });
+  await browser.close();
+}
