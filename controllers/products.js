5a86ee6f04e7234745a4a240084f7ce00a9fa835
edvicaty <edvicaty@hotmail.com>
2020-09-09 15:06:24 -0500

    store model added

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,7 @@
 const Product = require("../models/Product");
 const Review = require("../models/Review");
 const Location = require("../models/Location");
+const Store = require("../models/Store");
 const axios = require("axios");
 
 exports.listAllProducts = async (req, res) => {
@@ -24,7 +25,7 @@ exports.viewNewForm = (req, res) => {
 };
 
 exports.createProduct = async (req, res) => {
-  const { name, type, store, priceProfeco } = req.body;
+  const { name, type, store, priceProfeco, imageUrl } = req.body;
   //mapbox
   let minLong = "-100.00";
   let minLat = "19.00";
@@ -41,17 +42,18 @@ exports.createProduct = async (req, res) => {
       lat: response.data.features[0].center[1],
     },
   });
-  let appendStore = {
+
+  const newStore = await Store.create({
     storeName: store,
     priceProfeco,
     priceUser: [],
     locations: location,
-  };
-
+  });
   await Product.create({
     name,
     type,
-    stores: appendStore,
+    stores: newStore,
+    imageUrl,
   });
 
   res.redirect("/");
@@ -85,14 +87,23 @@ exports.viewProductType = async (req, res) => {
 
 exports.viewProduct = async (req, res) => {
   const id = req.params.id;
-  const product = await Product.findById(id).populate("stores.priceUser");
+  const product = await Product.findById(id).populate({
+    path: "stores",
+    populate: {
+      path: "priceUser",
+      model: "Review",
+    },
+  });
+  //aqui vamos, falta decidir sobre profeco o no
   arrayPricesProfeco = [];
   product.stores.forEach((store) => {
     arrayPricesProfeco.push(store.priceProfeco);
   });
+
   averageProfeco =
     arrayPricesProfeco.reduce((acc, currentValue) => acc + currentValue) /
     arrayPricesProfeco.length;
+
   maxProfeco = Math.max(arrayPricesProfeco);
   minProfeco = Math.min(arrayPricesProfeco);
   // console.log(averageProfeco, maxProfeco, minProfeco);
@@ -104,31 +115,32 @@ exports.viewProduct = async (req, res) => {
 };
 
 exports.updateUserReview = async (req, res) => {
-  const { productId, storeName } = req.params;
+  const { storeId, productId } = req.params;
   const { score } = req.body;
+
   const review = await Review.create({ score, user: req.user.id });
-  let product = await Product.findOne({ _id: productId });
-  let productStores = product.stores;
-  let specificStoreArray = productStores.filter(
-    (store) => store.storeName === storeName
-  );
 
-  specificStoreArray[0].priceUser.push(review._id);
-  await product.save();
+  const store = await Store.findOne({ _id: storeId });
+  store.priceUser.push(review);
+  store.save();
 
-  //for para las tiendas
-  let updatedProduct = await Product.findOne({ _id: productId }).populate(
-    "stores.priceUser"
-  );
+  let updatedProduct = await Product.findOne({ _id: productId }).populate({
+    path: "stores",
+    populate: {
+      path: "priceUser",
+      model: "Review",
+    },
+  });
 
   for (let i = 0; i < updatedProduct.stores.length; i++) {
     let sum = 0;
     for (let j = 0; j < updatedProduct.stores[i].priceUser.length; j++) {
       sum += updatedProduct.stores[i].priceUser[j].score;
     }
+    console.log(updatedProduct.stores[i]);
     updatedProduct.stores[i].average =
       sum / updatedProduct.stores[i].priceUser.length;
-    await updatedProduct.save();
+    await updatedProduct.stores[i].save();
   }
   res.redirect(`/detail/${productId}`);
 };
--- controllers/stores.js
+++ controllers/stores.js
@@ -1,1 +1,41 @@
-exports.viewStoreForm = async (req, res) => {};
+const Product = require("../models/Product");
+const Store = require("../models/Store");
+const Location = require("../models/Location");
+const axios = require("axios");
+
+exports.viewStoreForm = async (req, res) => {
+  const { productId } = req.params;
+  const product = await Product.findById(productId);
+  res.render("stores/new", product);
+};
+exports.createStore = async (req, res) => {
+  const { productId } = req.params;
+  const { storeTitle, storeColonia, storeMunicipality } = req.body;
+  const mapQuery = `${storeTitle}%20Colonia%20${storeColonia}%20Delegacion%20${storeMunicipality}%20`;
+  let minLong = "-100.00";
+  let minLat = "19.00";
+  let maxLong = "-98.00";
+  let maxLat = "20.00";
+  let mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${mapQuery}.json?bbox=${minLong},${minLat},${maxLong},${maxLat}&access_token=pk.eyJ1IjoiZWR2aWNhdHkiLCJhIjoiY2tla2tkaHZ6MDg3ODJxbXN2aW9ldnVmbCJ9.jzrSUZ18F2b4FErS8pHTGA`;
+  console.log(mapBoxUrl);
+  let response = await axios.get(mapBoxUrl);
+  const location = await Location.create({
+    municipality: response.data.features[2].context[1].text,
+    coordinates: {
+      long: response.data.features[0].center[0],
+      lat: response.data.features[0].center[1],
+    },
+  });
+  const newStore = await Store.create({
+    storeName: `${storeTitle}, ${storeColonia}, ${storeMunicipality}`,
+    priceUser: [],
+    locations: location,
+  });
+  const product = await Product.findOne({ _id: productId });
+  product.stores.push(newStore);
+  product.save();
+  res.redirect(`/detail/${productId}`);
+};
+exports.viewEditForm = async (req, res) => {};
+exports.editForm = async (req, res) => {};
+exports.deleteStore = async (req, res) => {};
