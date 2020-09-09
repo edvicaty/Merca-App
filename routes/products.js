627f06056442d3737e50cf7aab0f942bbba9b2dc
edvicaty <edvicaty@hotmail.com>
2020-09-08 19:42:07 -0500

    add mapbox with queries for creating stores

--- routes/products.js
+++ routes/products.js
@@ -22,5 +22,8 @@ router.get("/delete/:id", isAuth, checkRole("ADMIN"), deleteProduct);
 router.get("/type/:type", isAuth, viewProductType);
 router.get("/detail/:id", isAuth, viewProduct);
 router.post("/updatePriceUser/:productId/:storeName", isAuth, updateUserReview);
+router.get(
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/:productName.json?access_token=pk.eyJ1IjoiZWR2aWNhdHkiLCJhIjoiY2tla2tkaHZ6MDg3ODJxbXN2aW9ldnVmbCJ9.jzrSUZ18F2b4FErS8pHTGA"
+);
 
 module.exports = router;
