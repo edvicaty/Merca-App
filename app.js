da3072bb6942b8a65bac82bc07e57b3c14c73af2
edvicaty <edvicaty@hotmail.com>
2020-09-10 12:09:23 -0500

    add verify feature

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 
 mongoose
-  .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
