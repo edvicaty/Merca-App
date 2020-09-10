c0778e7ac6ea54bf848b99ab14aa571c87e893cf
edvicaty <edvicaty@hotmail.com>
2020-09-10 08:55:04 -0500

    add verification routes and navbar auth layout

--- routes/stores.js
+++ routes/stores.js
@@ -3,15 +3,16 @@ const router = express.Router();
 const {
   viewStoreForm,
   createStore,
-  viewEditForm,
-  editForm,
+  viewVerifyForm,
+  verifyForm,
   deleteStore,
 } = require("../controllers/stores");
 const { isAuth, checkRole, catchErrors } = require("../middlewares");
 router.get("/store/new/:productId", isAuth, viewStoreForm);
 router.post("/store/new/:productId", isAuth, createStore);
-router.get("/store/edit/:storeId", isAuth, viewEditForm);
-router.post("/store/edit/:storeId", isAuth, editForm);
-router.get("/store/delete/:storeId", isAuth, deleteStore);
+//FALTA VERIFICAR TIENDAS
+router.get("/store/checkVerify", isAuth, checkRole("ADMIN"), viewVerifyForm);
+router.post("/store/verify/:storeId", isAuth, checkRole("ADMIN"), verifyForm);
+router.get("/store/delete/:storeId", isAuth, checkRole("ADMIN"), deleteStore);
 
 module.exports = router;
